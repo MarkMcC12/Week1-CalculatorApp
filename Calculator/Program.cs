f50693637c2d83d8c4a5f90d310ccde6b31f1660
Mark McConnell <McConnell-M22@ulster.ac.uk>
2024-09-24 14:51:07 +0100

    created calculator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// This stores the first number from the user 
+Console.WriteLine("Pleaase type in the first number, then hit the enter key");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// this stores, and converts the second value from the user
+Console.WriteLine("Pleaase type in the second number, then hit the enter key");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// calculating answer 
+int result = firstNumber + secondNumber;
+Console.WriteLine("Adding {0} and {1} will give the answer of {2}", firstNumber, secondNumber, result);
+
